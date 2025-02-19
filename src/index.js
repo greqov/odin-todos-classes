31f0d6ec85fa122a244f1d25dba8f29de032e603
Ivan Greqov <idgreq@bk.ru>
2025-02-19 15:52:40 +0300

    Add default todos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './reset.css';
 import './styles.css';
 
+import defaultData from './defaultData.json';
 import Project from './Project';
 import Todo from './Todo';
 
 const project = new Project({ title: 'Default' });
 const todo = new Todo({ title: 'the first one', priority: 'high' });
 
-project.addTodo(todo);
+defaultData.forEach((todo) => {
+  project.addTodo(new Todo(todo));
+});
 
 console.log(project);
 console.log(todo);
