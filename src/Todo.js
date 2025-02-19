d1fa26aa15dfdb83c1a06c78d125b45d23306e70
Ivan Greqov <idgreq@bk.ru>
2025-02-19 16:54:58 +0300

    Add status prop in Todo class

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,14 @@
 export default class Todo {
   constructor({
     id = `todo-${crypto.randomUUID()}`,
+    done = false,
     title = 'untitled',
     description = '',
     dueDate = new Date(),
     priority = 'middle'
   }) {
     this.id = id;
+    this.done = done;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
